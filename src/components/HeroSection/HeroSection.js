e4f4bb21ad26cf18e345048b9d3898d7c9b032ec
nhussein2026 <naser738nas@gmail.com>
2022-11-08 02:14:59 +0300

    HeroSection & assets

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,9 +1,18 @@
-import "./HeroSection.css"
+import "./HeroSection.css";
+import img from "../../asssets/images/hero.jpg"
 const HeroSection = () => {
 
     return(
-        <div className="hero">
-            
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+
+                </div>
+                <div className="col-6">
+                    <img id="hero-img" src={img} alt="can not found the image" />
+                </div>
+                
+            </div>
         </div>
     )
 }
