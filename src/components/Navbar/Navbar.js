2107ad257fc75f29c3c257ea63025008697c254e
nhussein2026 <naser738nas@gmail.com>
2022-11-10 00:30:07 +0300

    fixed logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom"
 import  "./Navbar.css"
-import logo from "../../assets/images/icons/Safha-logo.jpeg"
+import logo from "../../assets/images/logo.png"
 const Navbar = () => {
     const links = [
         {
